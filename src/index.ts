5e0898118b72d97b89ee74e99076c4c8b914373a
Iroha1024 <47475495+Iroha1024@users.noreply.github.com>
2021-03-06 17:58:13 +0800

    feat: preview

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,10 @@ interface TreeData {
 type SortTreeData = Pick<TreeData, 'uuid' | 'next' | 'rank'>
 
 export default class StoryTree {
+    scale = 0
+
     stage: Konva.Stage
+    preview?: Konva.Stage
     data: TreeData[]
 
     constructor(option: Option) {
@@ -48,6 +51,7 @@ export default class StoryTree {
             height,
         })
         this.draw(canvasWidth, canvasHeight)
+        this.drawPreview(canvasWidth, canvasHeight)
     }
 
     lineUp(root: Node) {
@@ -81,7 +85,7 @@ export default class StoryTree {
     setPosition(square: SortTreeData[][], option: LocateOption) {
         const nodeWidth = 100,
             nodeHeight = 80
-        const { width, padding, gutter } = option
+        const { padding, gutter } = option
         const paddingX =
             padding == undefined ? 0 : typeof padding == 'number' ? padding : padding[0]
         const paddingY =
@@ -119,9 +123,6 @@ export default class StoryTree {
         temp.forEach((item) => {
             item.x += maxCoordinateX + paddingX
             item.y += paddingY
-            if (width > actualCanvasWidth) {
-                item.x += (width - actualCanvasWidth) / 2
-            }
         })
 
         return {
@@ -132,24 +133,23 @@ export default class StoryTree {
     }
 
     draw(canvasWidth: number, canvasHeight: number) {
-        const layer = new Konva.Layer()
-        this.stage.add(layer)
+        const stage = this.stage
+
+        const layer = new Konva.Layer({
+            id: 'mainLayer',
+        })
+        stage.add(layer)
 
         const background = new Konva.Rect({
-            x: 0,
-            y: 0,
-            width: this.stage.width(),
-            height: this.stage.height(),
+            width: stage.width(),
+            height: stage.height(),
             fill: '#c5e5f6',
             listening: false,
         })
         layer.add(background)
 
-        const stage = this.stage
-
         const group = new Konva.Group({
-            x: 0,
-            y: 0,
+            id: 'nodeGroup',
             draggable: stage.width() < canvasWidth || stage.height() < canvasHeight,
             dragBoundFunc(pos) {
                 let position = {
@@ -178,6 +178,32 @@ export default class StoryTree {
             },
         })
 
+        //x center
+        if (stage.width() > canvasWidth) {
+            group.setAttrs({
+                offsetX: (canvasWidth - stage.width()) / 2,
+            })
+        }
+
+        group.on('dragmove', () => {
+            if (this.preview) {
+                const previewLayer = this.preview.findOne('#previewLayer')
+                const slider = (previewLayer as Konva.Layer).findOne('#slider')
+                slider.setAttrs({
+                    x: -group.x() * this.scale,
+                    y: -group.y() * this.scale,
+                })
+                ;(previewLayer as Konva.Layer).batchDraw()
+            }
+        })
+
+        this.drawShape(group)
+
+        layer.add(group)
+        layer.draw()
+    }
+
+    drawShape(group: Konva.Group) {
         this.data.forEach((node) => {
             const { x, y, width, height } = node
             const rect = new Konva.Rect({
@@ -206,8 +232,139 @@ export default class StoryTree {
                 }
             })
         })
+    }
 
+    drawPreview(canvasWidth: number, canvasHeight: number) {
+        const stage = this.stage
+
+        let scale = 1 / Math.max(canvasWidth / stage.width(), canvasHeight / stage.height())
+        if (scale > 1) return
+        scale *= 0.3
+
+        this.scale = scale
+
+        const container = stage.container()
+        Object.assign(container.style, {
+            position: 'relative',
+            width: 'fit-content',
+        })
+
+        const preview = document.createElement('div')
+        preview.id = 'preview'
+        Object.assign(preview.style, {
+            position: 'absolute',
+            top: '0',
+            right: '0',
+        })
+        container.append(preview)
+
+        const previewStage = new Konva.Stage({
+            container: preview,
+            width: scale * canvasWidth,
+            height: scale * canvasHeight,
+        })
+
+        previewStage.on('click', () => {
+            const verctor = previewStage.getPointerPosition()
+            if (verctor == null) return
+            let { x, y } = verctor
+            const halfSliderWidth = (scale * stage.width()) / 2
+            const halfSliderHeight = (scale * stage.height()) / 2
+            const sliderCenterPoint = {
+                x: slider.x() + halfSliderWidth,
+                y: slider.y() + halfSliderHeight,
+            }
+            if (x > scale * canvasWidth - halfSliderWidth) {
+                x = scale * canvasWidth - halfSliderWidth
+            } else if (x < halfSliderWidth) {
+                x = halfSliderWidth
+            }
+            if (y > scale * canvasHeight - halfSliderHeight) {
+                y = scale * canvasHeight - halfSliderHeight
+            } else if (y < halfSliderHeight) {
+                y = halfSliderHeight
+            }
+            slider.setAttrs({
+                x: slider.x() + x - sliderCenterPoint.x,
+                y: slider.y() + y - sliderCenterPoint.y,
+            })
+            layer.batchDraw()
+            moveNodeGroup()
+        })
+
+        this.preview = previewStage
+
+        const layer = new Konva.Layer({
+            id: 'previewLayer',
+        })
+        previewStage.add(layer)
+
+        const background = new Konva.Rect({
+            width: previewStage.width(),
+            height: previewStage.height(),
+            fill: '#f0f0f080',
+            listening: false,
+        })
+        layer.add(background)
+
+        const mainLayer = stage.findOne('#mainLayer')
+        const nodeGroup = (mainLayer as Konva.Layer).findOne('#nodeGroup')
+        const group = new Konva.Group({
+            scaleX: scale,
+            scaleY: scale,
+        })
+        Array.from(nodeGroup.children).forEach((item) => {
+            const shape = item as Konva.Shape
+            const clone = shape.clone()
+            group.add(clone)
+        })
         layer.add(group)
+
+        const slider = new Konva.Rect({
+            width: scale * stage.width(),
+            height: scale * stage.height(),
+            id: 'slider',
+            fill: '#cccccc80',
+            draggable: stage.width() < canvasWidth || stage.height() < canvasHeight,
+            dragBoundFunc(pos) {
+                let position = {
+                    x: pos.x,
+                    y: pos.y,
+                }
+                if (stage.width() >= canvasWidth) {
+                    position.x = this.absolutePosition().x
+                } else {
+                    if (pos.x < 0) {
+                        position.x = 0
+                    } else if (pos.x > (canvasWidth - stage.width()) * scale) {
+                        position.x = (canvasWidth - stage.width()) * scale
+                    }
+                }
+                if (stage.height() >= canvasHeight) {
+                    position.y = this.absolutePosition().y
+                } else {
+                    if (pos.y < 0) {
+                        position.y = 0
+                    } else if (pos.y > (canvasHeight - stage.height()) * scale) {
+                        position.y = (canvasHeight - stage.height()) * scale
+                    }
+                }
+                return position
+            },
+        })
+
+        const moveNodeGroup = () => {
+            nodeGroup.setAttrs({
+                x: -slider.x() / scale,
+                y: -slider.y() / scale,
+            })
+            ;(mainLayer as Konva.Layer).batchDraw()
+        }
+
+        slider.on('dragmove', moveNodeGroup)
+
+        layer.add(slider)
+
         layer.draw()
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,10 @@
 import StoryTree from '../src/index'
 
-const node5 = {
-    uuid: '5',
+const node6 = {
+    uuid: '6',
     next: [
         {
-            uuid: '6',
+            uuid: '7',
             next: [],
         },
     ],
@@ -14,20 +14,27 @@ new StoryTree({
     container: '#app',
     width: 400,
     height: 400,
+    // width: window.innerWidth,
+    // height: window.innerHeight,
     root: {
         uuid: '1',
         next: [
             {
                 uuid: '2',
-                next: [node5],
+                next: [node6],
             },
             {
                 uuid: '3',
-                next: [node5],
+                next: [node6],
             },
             {
                 uuid: '4',
-                next: [node5],
+                next: [
+                    {
+                        uuid: '5',
+                        next: [node6],
+                    },
+                ],
             },
         ],
     },
